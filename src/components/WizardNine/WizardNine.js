2d96a21cfc0ddd26e6f4d679b2686f5776e042b0
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-10-04 08:58:34 -0600

    more changes

--- src/components/WizardNine/WizardNine.js
+++ src/components/WizardNine/WizardNine.js
@@ -1,25 +1,35 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import { updateAddress1, updateAddress2, updateAddress3 } from '../../ducks/reducer.js';
 class WizardNine extends Component {
 
-    render(){
-        return(
+    render() {
+        const { address1, address2, address3 } = this.props;
+        return (
             <div className="parent-div">
-                <div className="vert-align">                    
-                    
+                <div className="vert-align">
+
                     <p>What is your address?</p> <br />
 
-                    <input type="text" placeholder="Line One" onChange={this.props.updateAddLineOne}/>
-                    <input type="text" placeholder="Line Two" onChange={this.props.updateAddLineTwo}/>
-                    <input type="text" placeholder="Line Three" onChange={this.props.updateAddLineThree}/>
-                
-                    
+                    <input type="text" placeholder="Line One" onChange={e => updateAddress1(e.target.value)} />
+                    <input type="text" placeholder="Line Two" onChange={e => updateAddress2(e.target.value)} />
+                    <input type="text" placeholder="Line Three" onChange={e => updateAddress3(e.target.value)} />
+
+
                     <Link to="/wTen"><button className="margin-btn"> Next </button></Link>
                 </div>
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    const { address1, address2, address3 } = state;
+    return {
+        address1,
+        address2,
+        address3
+    }
+}
 
-export default WizardNine;
+export default connect(mapStateToProps, { updateAddress1, updateAddress2, updateAddress3 })(WizardNine);
